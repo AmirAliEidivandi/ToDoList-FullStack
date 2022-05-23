620d95be6bade574c3c614ccee23f2a261738bb1
AmirAliEidivandi <amirah648887@gmail.com>
2022-05-23 21:05:49 +0430

    but fix

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
-const connection = require("./db");
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require('dotenv').config()
 
-connection();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Listening on port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
